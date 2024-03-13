0f5349731eaf60d6ed47626ca0eed6e5a3df7b10
jamesdank <94645949+jamesdank@users.noreply.github.com>
2024-03-13 12:10:51 -0500

    Create mysqldump.sh

---
+++ mysqldump.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+db_username="root"
+
+db_password=""
+
+destination="path/to/backup/dir"
+
+date="$(date +%Y-%m-%d_%H%M%S)"
+
+mysqldump=$(whereis mysqldump | sed 's|mysqldump: ||g')
+
+mkdir -p "$destination/backup-$date"
+
+# Backup Databases
+declare -a databases=(
+    'database1'
+    'database2'
+    'database3'
+    );
+
+# Create Database Backups
+for db in ${databases[@]}
+do
+	$mysqldump -u $db_username -p "--password=$db_password" "$db" > "$destination/backup-$date/db-$db.sql"
+
+    echo "$db" Database Backup Created.
+done
